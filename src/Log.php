2a85cb8628afa9b7fb60be99f5e4b7b540f93fb4
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2022-12-07 21:41:24 +0100

    Update Log.php

--- src/Log.php
+++ src/Log.php
@@ -175,7 +175,7 @@ class Log {
 
     public static function source(){
 
-        return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        return '';
 
     }
 
